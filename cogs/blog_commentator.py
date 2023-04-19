e9d4922f82be7a487d1a6ffa702c965844d763fe
Kliment Semushev <opened.chest@gmail.com>
2023-04-19 22:08:20 +0300

    comments

---
+++ cogs/blog_commentator.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+import server
+
+
+class blog_commentator(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        print('blog_commentator started!')
+        
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        guild = await server.get_data()
+        if (message.channel.category_id == guild['blogs']) & (len(message.content) > 0):
+            try:
+                await message.create_thread(name=message.content[:95], reason="Комментарии")
+            except Exception as e:
+                print(e)
+        
+async def setup(bot):
+    await bot.add_cog(blog_commentator(bot))
+    
