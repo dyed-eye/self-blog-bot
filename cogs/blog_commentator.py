04a003aebfbdf0a7f19899f7e2c57cddbdef6a94
Kliment Semushev <opened.chest@gmail.com>
2023-04-20 16:05:23 +0300

    no comments for setup channels

--- cogs/blog_commentator.py
+++ cogs/blog_commentator.py
@@ -11,7 +11,7 @@ class blog_commentator(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         guild = await server.get_data()
-        if (message.channel.category_id == guild['blogs']) & (len(message.content) > 0) & (type(message.channel) != Thread):
+        if (message.channel.category_id == guild['blogs']) & (len(message.content) > 0) & (type(message.channel) != Thread) & (message.channel.id not in [guild['channel_moderation'], guild['channel_start'], guild['channel_presentation']]:
             try:
                 await message.create_thread(name=message.content[:95], reason="Комментарии")
             except Exception as e:
